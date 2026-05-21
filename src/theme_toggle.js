0c2958a027ec12dcf27e6457c72ff1b9511852dd
Chris Strawser <46349138+mcochris@users.noreply.github.com>
2026-05-21 14:51:28 -0400

    Check that btn exists as per console error

--- src/theme_toggle.js
+++ src/theme_toggle.js
@@ -1,12 +1,14 @@
 const btn = document.getElementById('theme-toggle');
 
-btn.addEventListener('click', () => {
-	const currentTheme = document.documentElement.getAttribute('data-bs-theme');
-	const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+if (btn) {
+	btn.addEventListener('click', () => {
+		const currentTheme = document.documentElement.getAttribute('data-bs-theme');
+		const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
 
-	document.documentElement.setAttribute('data-bs-theme', newTheme);
-	localStorage.setItem('theme', newTheme); // Remember choice
-});
+		document.documentElement.setAttribute('data-bs-theme', newTheme);
+		localStorage.setItem('theme', newTheme); // Remember choice
+	});
+}
 
 // Apply saved theme on page load
 const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
